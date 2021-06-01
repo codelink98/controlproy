b87c5d90b9aa1c6f4174ae3b86d6f6eb29e8d47e
Martin Torres <martintb@live.com.mx>
2021-06-01 01:38:14 -0500

    cookieS

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function loginAdmin() {
     if(document.getElementById("login").value === "martintb@live.com.mx" && document.getElementById("password").value === "Nutrifit10") {
 
         alert("Usuario correcto!")
+        setCookie("usuario", "Martin Torres Becerra", 1);
         window.location = "home.html";
 
     } else {
@@ -15,3 +16,26 @@ function loginAdmin() {
 }
 
 console.log("index js loaded")
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
