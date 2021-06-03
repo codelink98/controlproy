9c64fe098a67a938cf39d4a459d06a1bcf0ac828
Martin Torres <martintb@live.com.mx>
2021-06-03 02:47:43 -0500

    Eliminate AWS Credentials

--- upload_dieta.php
+++ upload_dieta.php
@@ -1,8 +1,6 @@
 <?php
-        $credentials = new Aws\Credentials\Credentials('AKIA57TDT64IUZSM7Z5L', '/JY+fxprcX6zSHhwClZyHKejP1fViC3zf3IdHf2n');
         $s3 = new Aws\S3\S3Client([
             'version'     => 'latest',
-            'region'      => 'us-west-2',
-            'credentials' => $credentials
+            'region'      => 'us-west-2'
         ]);
     ?> 
