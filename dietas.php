5c2092def3ef731bce4d35925fb2b1eefda64c87
Martin Torres <martintb@live.com.mx>
2021-06-03 03:03:12 -0500

    Eliminate AWS Credentials

--- dietas.php
+++ dietas.php
@@ -50,7 +50,7 @@
       <h1>Usuarios</h1>
     </div>
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <form action="upload_dieta.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
--- upload_dieta.php
+++ upload_dieta.php
@@ -1,6 +1,13 @@
 <?php
-        $s3 = new Aws\S3\S3Client([
-            'version'     => 'latest',
-            'region'      => 'us-west-2'
-        ]);
-    ?> 
+    // Require the Composer autoloader.
+    require 'vendor/autoload.php';
+
+    use Aws\S3\S3Client;
+
+    $s3 = new Aws\S3\S3Client([
+        'version'     => 'latest',
+        'region'      => 'us-west-2'
+    ]);
+
+    var_dump($_FILES["fileToUpload"]["name"])
+?> 
