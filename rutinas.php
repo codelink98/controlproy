f3748117304c3711863c84e68de0fdba0850c7f9
Martin Torres <martintb@live.com.mx>
2021-06-03 12:16:05 -0500

    Making resposive

--- rutinas.php
+++ rutinas.php
@@ -19,17 +19,17 @@
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
-        </button>
+        </button> -->
         <a class="navbar-brand" href="#">NutriFit</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> -->
         <ul class="nav navbar-nav">
           <li><a href="home.html">Home <span class="sr-only">(current)</span></a></li>
           <li><a href="usuarios.php">Usuarios</a></li>
@@ -37,7 +37,7 @@
           <li class="active"><a href="#">Rutinas</a></li>
         </ul>
       </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+    <!-- </div>/.container-fluid -->
   </nav>
 
   <body>
