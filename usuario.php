8b18fac94e6054eccc136daf08688b32976dd9d1
Martin Torres <martintb@live.com.mx>
2021-06-23 23:41:23 -0500

    changing the css

--- usuario.php
+++ usuario.php
@@ -57,7 +57,7 @@
 
     if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - email: " . $row["email"];
+        //echo "id: " . $row["id"]. " - email: " . $row["email"];
       }
     } 
 
