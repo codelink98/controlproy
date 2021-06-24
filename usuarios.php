1530e2164877bbab6b17c00a6989de2a1de504cf
Martin Torres <martintb@live.com.mx>
2021-06-24 11:26:08 -0500

    better list

--- usuarios.php
+++ usuarios.php
@@ -74,8 +74,44 @@
       } 
 
       $conn->close();
-
     ?>
+
+    <form>
+      <div class="form-group">
+        <label for="id">ID del usuario</label>
+        <input type="number" class="form-control" id="id_user">
+      </div>
+      <div class="form-group">
+        <label for="cintura">Cintura</label>
+        <input type="text" class="form-control" id="cintura">
+      </div>
+      </div>
+      <div class="form-group">
+        <label for="abdomen">Abdomen</label>
+        <input type="text" class="form-control" id="abdomen">
+      </div>
+      </div>
+      <div class="form-group">
+        <label for="peso">Peso</label>
+        <input type="text" class="form-control" id="peso">
+      </div>
+      </div>
+      <div class="form-group">
+        <label for="estatura">Estatura</label>
+        <input type="text" class="form-control" id="estatura">
+      </div>
+      </div>
+      <div class="form-group">
+        <label for="grasa">Grasa</label>
+        <input type="text" class="form-control" id="grasa">
+      </div>
+      </div>
+      <div class="form-group">
+        <label for="masa_muscular">Masa muscular</label>
+        <input type="text" class="form-control" id="masa_muscular">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
   </body>
 </html>
